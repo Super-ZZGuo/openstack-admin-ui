a1a84cbc6cae46d654398e9eef54847cabda4ffe
Radishaa <1536857028@qq.com>
2023-03-26 18:12:33 +0800

    fix: range view select diy contribute

--- src/views/admin/sys-project/index.vue
+++ src/views/admin/sys-project/index.vue
@@ -63,7 +63,7 @@
                   :data="sysRangeList"
                   border
                 >
-                  <el-table-column label="靶场名称" prop="rangeName" width="100" :show-overflow-tooltip="true" />
+                  <el-table-column label="靶机名称" prop="rangeName" width="100" :show-overflow-tooltip="true" />
                   <el-table-column label="状态" prop="status" width="80" />
                   <el-table-column label="镜像" prop="image" width="80" />
                   <el-table-column label="实例类型" prop="flavor" width="80" />
@@ -83,6 +83,14 @@
 
           <el-table-column label="操作" align="center" width="180" class-name="small-padding fixed-width">
             <template slot-scope="scope">
+
+              <el-button
+                v-permisaction="['admin:sysImge:edit']"
+                size="mini"
+                type="text"
+                icon="el-icon-edit"
+                @click="handleUpdate(scope.row)"
+              >修改</el-button>
 
               <el-button
                 v-if="scope.row.p_id != 0"
@@ -109,24 +117,75 @@
           ref="drawer"
           :title="title"
           :before-close="cancel"
-          :visible.sync="open"
+          :visible.sync="open_edit"
           direction="rtl"
           custom-class="demo-drawer"
-          size="830px"
+          size="70%"
         >
-          <el-table
-            v-loading="loading_sub"
-            :data="sysRangeList"
-            size:="mini"
-          >
-            <el-table-column label="靶场名称" prop="rangeName" width="100" :show-overflow-tooltip="true" />
-            <el-table-column label="状态" prop="status" width="80" />
-            <el-table-column label="镜像" prop="image" width="80" />
-            <el-table-column label="实例类型" prop="flavor" width="80" />
-            <el-table-column label="标签" prop="tag" :show-overflow-tooltip="true" />
-          </el-table>
+          <el-row class="mb8">
+            <el-col>
+              <el-radio-group v-model="radio">
+                <el-radio-button label="1">自定义生成靶场</el-radio-button>
+                <el-radio-button label="2">格式化生成靶场</el-radio-button>
+              </el-radio-group>
+            </el-col>
+          </el-row>
 
+          <div v-if="radio==1">
+
+            <el-row class="mb8">
+              <el-col :span="22">
+                <el-alert
+                  title="警告:在创建靶机之前需要创建网络"
+                  type="warning"
+                  description="创建靶机时，确认您填写的想要将靶机连接的到网络已经创建完毕或者已经存在。在选择网络之后填写固定IP(如果需要的话)，选择固定IP之后无法修改！"
+                  :closable="false"
+                  show-icon
+                />
+              </el-col>
+            </el-row>
+            <el-row class="mb8">
+              <el-col :span="22">
+                <el-table
+                  v-loading="loading_sub"
+                  :data="sysRangeList"
+                  size:="mini"
+                >
+                  <el-table-column label="靶场名称" prop="rangeName" width="100" :show-overflow-tooltip="true" />
+                  <el-table-column label="状态" prop="status" width="80" />
+                  <el-table-column label="镜像" prop="image" width="80" />
+                  <el-table-column label="实例类型" prop="flavor" width="80" />
+                  <el-table-column label="标签" prop="tag" :show-overflow-tooltip="true" />
+                </el-table>
+              </el-col>
+            </el-row>
+
+          </div>
         </el-drawer>
+
+        <!-- 新增靶场 -->
+        <el-dialog :title="title" :visible.sync="open_add" width="600px" :close-on-click-modal="false">
+          <el-form ref="form" :model="form" :rules="rules" label-width="150px">
+            <el-row>
+              <el-col :span="24">
+                <el-form-item label="靶场名称" prop="projectName">
+                  <el-input v-model="form.projectName" placeholder="请输入靶场名称" />
+                </el-form-item>
+              </el-col>
+
+              <el-col :span="24">
+                <el-form-item label="标签" prop="tag">
+                  <el-input v-model="form.tag" type="textarea" placeholder="请输入标签" />
+                </el-form-item>
+              </el-col>
+            </el-row>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button type="primary" @click="submitForm">确 定</el-button>
+            <el-button @click="cancel">取 消</el-button>
+          </div>
+        </el-dialog>
+
       </el-card>
     </template>
   </BasicLayout>
@@ -134,7 +193,7 @@
 
 <script>
 import { addSysProject, delSysProject, listSysProject, updateSysProject } from '@/api/admin/sys-project'
-import { listSysRange } from '@/api/admin/sys-project'
+import { listSysRange, updateSysRange } from '@/api/admin/sys-project'
 
 export default {
   name: 'SysProject',
@@ -156,7 +215,8 @@ export default {
       // 弹出层标题
       title: '',
       // 是否显示弹出层
-      open: false,
+      open_edit: false,
+      open_add: false,
       isEdit: false,
       isEditO: false,
       // slot: "",
@@ -168,7 +228,7 @@ export default {
         projectName: '',
         projectId: ''
       },
-
+      radio: 1,
       // 靶场类型选项
       sysImgOptions: [{
         value: 'ami',
@@ -204,6 +264,11 @@ export default {
         projectNewName: '',
         oldTag: '',
         newTag: ''
+      },
+      sysRangeStutusUpdate: {
+        projectName: '',
+        rangeName: '',
+        option: ''
       },
       // 关系表类型
 
@@ -244,11 +309,12 @@ export default {
     },
     // 取消按钮
     cancel() {
-      this.open = false
-      this.reset()
+      this.open_add = false
+      this.open_edit = false
+      this.reset_project()
     },
     // 表单重置
-    reset() {
+    reset_project() {
       this.form = {
         projectId: undefined,
         projectName: undefined,
@@ -256,9 +322,6 @@ export default {
       }
       this.resetForm('form')
     },
-    // getImgList: function() {
-    //   this.form[this.fileIndex] = this.$refs['fileChoose'].resultList[0].fullUrl
-    // },
     fileClose: function() {
       this.fileOpen = false
     },
@@ -275,13 +338,14 @@ export default {
       this.resetForm('queryForm')
       this.handleQuery()
     },
+    checkStatus() {
+
+    },
     /** 新增按钮操作 */
     handleProjectAdd() {
-      this.reset()
-      this.open = true
+      this.reset_project()
+      this.open_add = true
       this.title = '添加靶场'
-      this.isEdit = false
-      this.isEditO = true
     },
     // 多选框选中数据
     handleSelectionChange(selection) {
@@ -291,16 +355,35 @@ export default {
     },
     /** 查看靶机按钮操作 */
     handleCheckRanges(row) {
-      this.reset()
+      this.reset_project()
       this.queryParams.projectName = row.projectName
+      this.loading_sub = true
       this.getRangeList()
+      for (var i = 0; i < this.sysRangeList.length; i++) {
+        this.sysRangeStutusUpdate.projectName = row.projectName
+        this.sysRangeStutusUpdate.rangeName = this.sysRangeList[i].rangeName
+
+        updateSysRange(this.sysRangeList[i].rangeId, this.sysRangeStutusUpdate).then(response => {
+          if (response.code === 200) {
+            this.msgSuccess('靶机状态' + response.msg)
+          } else {
+            this.msgError(response.msg)
+          }
+        })
+        this.sysRangeStutusUpdate.projectName = ''
+        this.sysRangeStutusUpdate.rangeName = ''
+      }
+      this.loading_sub = false
+      this.dateRange = []
+      this.resetForm('queryForm')
+      this.queryParams.pageIndex = 1
     },
     /** 修改按钮操作 */
     handleUpdate(row) {
-      this.reset()
-      this.getRangeList()
-      // this.open = true
-      // this.title = '修改靶场'
+      this.reset_project()
+      this.handleCheckRanges(row)
+      this.open_edit = true
+      this.title = '修改靶场'
     },
     /** 提交按钮 */
     submitForm: function() {
@@ -312,7 +395,7 @@ export default {
             updateSysProject(this.form.projectId, this.sysImgStructUpdate).then(response => {
               if (response.code === 200) {
                 this.msgSuccess(response.msg)
-                this.open = false
+                this.open_add = false
                 this.getList()
               } else {
                 this.msgError(response.msg)
@@ -322,7 +405,7 @@ export default {
             addSysProject(this.form).then(response => {
               if (response.code === 200) {
                 this.msgSuccess(response.msg)
-                this.open = false
+                this.open_add = false
                 this.getList()
               } else {
                 this.msgError(response.msg)
@@ -336,13 +419,13 @@ export default {
     handleDelete(row) {
       var Ids = (row.projectId && [row.projectId]) || this.ids
       var Names = (row.projectName && [row.projectName]) || this.ids.map(item => this.sysProjectList.find(subItem => subItem.projectId === item).projectName)
-      this.$confirm('是否确认删除名称为"' + Names + '"的靶场?', '警告', {
+      this.$confirm('此操作无法撤回！！！！！！！！！！！！是否确认删除名称为"' + Names + '"的靶场? ', '警告', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
       })
         .then(function() {
-          return delSysProject({ 'ids': Ids, 'names': Names })
+          return delSysProject({ 'ids': Ids, 'projectNames': Names })
         }).then((response) => {
           if (response.code === 200) {
             this.msgSuccess(response.msg)
